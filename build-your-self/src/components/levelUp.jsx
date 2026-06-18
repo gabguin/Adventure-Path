2b282a247c56a3ecedd3f4fc34ffde76873b58b7
Gab Guinocor <vincegabguinocor@gmail.com>
2026-06-18 10:54:01 +0800

    updated the level up system

---
+++ build-your-self/src/components/levelUp.jsx
@@ -0,0 +1,39 @@
+import './levelUp.css'
+import profilePic from '../assets/profile-pic.webp'
+export function LevelDisplay({ exp }) {
+  let requiredExp = 25;
+  let remainingExp = exp;
+  let level = 0;
+  let levelUp = Math.ceil(exp / 50);
+  while (remainingExp >= requiredExp) {
+    requiredExp += 50 * levelUp;
+    remainingExp = requiredExp - exp;
+    level = level + levelUp;
+  }
+  const barPercentage = (exp / requiredExp) * 100;
+  const circlePercentage = (exp / requiredExp) * 360;
+  return (
+    <div className='levelup-div'>
+      <div className='levelup-circle' style={{
+        background: `conic-gradient(
+        red ${circlePercentage}deg,
+        black 0deg)`}}>
+        <div className='inside-circle'>
+          <img src={profilePic} className='profile-pic'></img>
+        </div>
+      </div>
+      <div>
+        <div className='level-div'>
+          <div className='level-status' style={{
+            width: `${barPercentage}%`
+          }} >
+          </div>
+        </div>
+         <div className='level-up'>
+            <div>{exp} / {requiredExp}</div>
+            <div>{remainingExp} more until level up</div>
+          </div>
+      </div>
+    </div>
+  )
+}
--- build-your-self/src/pages/homepage.jsx
+++ build-your-self/src/pages/homepage.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
 import { AddingQuest } from "../components/addQuest";
 import { TimerQuest } from "../components/timer";
+import { LevelDisplay } from "../components/levelUp";
 export function HomePage() {
+  const[exp, setExp] = useState(0);
   const [openQuest, setOpenQuest] = useState(false);
   const [quest, setQuest] = useState([]);
 
   return (
     <>
+    <LevelDisplay exp={exp}></LevelDisplay>
       <button
         onClick={() => setOpenQuest(true)}>
         Add Quest
@@ -26,6 +29,10 @@ export function HomePage() {
               questName={quest.questName}
               duration={quest.duration}
               description={quest.description}
+              setExp={setExp}
+              deleteThis={ () =>
+                setQuest(prev => prev.filter((_, i) => i !== index))
+              }
             ></TimerQuest>
           )
         })}
