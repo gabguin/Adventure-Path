9d9101f1d087fc951999c9bd794f93b1f376e3bc
Gab Guinocor <vincegabguinocor@gmail.com>
2026-06-18 11:32:09 +0800

    successfully change the import files

--- build-your-self/src/components/overlay.jsx
+++
@@ -1,8 +0,0 @@
-import './overlay.css'
-export function Overlay({children}){
-  return(
-    <div className="overlay-div">
-      {children}
-    </div>
-  )
-}
--- build-your-self/src/pages/homepage/homepage.jsx
+++ build-your-self/src/pages/homepage/homepage.jsx
@@ -1,32 +1,27 @@
 import { useState } from "react";
 import { AddingQuest } from "../../components/addquest/addQuest";
 import { LevelDisplay } from "../../components/levelup/levelUp";
+import {Routes, Route} from 'react-router'
 import "./homepage.css";
-
 export function HomePage() {
   const [exp, setExp] = useState(0);
   const [openQuest, setOpenQuest] = useState(false);
   const [quest, setQuest] = useState([]);
-
   return (
     <>
       <LevelDisplay exp={exp} />
-
       <button onClick={() => setOpenQuest(true)}>
         Add Quest
       </button>
-
       {openQuest && (
         <AddingQuest
           close={() => setOpenQuest(false)}
           setQuest={setQuest}
         />
       )}
-
       <div>
         <p>Quest in progress:</p>
       </div>
-
       {quest.map((item, index) => (
         <div
           key={index}
